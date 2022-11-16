4b91830387d72bcf7762cd97e3d7ff6adb711eb9
natalyVozna <109243941+natalyVozna@users.noreply.github.com>
2022-11-16 17:10:11 +0200

    refsctor

--- src/components/Section/Section.styled.jsx
+++ src/components/Section/Section.styled.jsx
@@ -1,13 +1,16 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  min-width: 600px;
-  min-height: 100vh;
+  width: 600px;
+  margin: 40px auto;
+
   display: flex;
   align-items: flex-start;
   padding: 40px;
   flex-direction: column;
   background-color: var(--green);
+  border-radius: 6px;
+  box-shadow: 1px 2px 6px 0px rgba(33, 33, 33, 0.5);
 `;
 export const Title = styled.h1`
   display: flex;
